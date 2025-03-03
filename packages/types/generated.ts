24148abc6b8e983f78b11785e115cdaef9f18277
benjamin.747 <benjamin.747@outlook.com>
2025-03-03 16:23:07 +0800

    update api call domain

--- packages/types/generated.ts
+++ packages/types/generated.ts
@@ -4247,7 +4247,7 @@ export enum ContentType {
 }
 
 export class HttpClient<SecurityDataType = unknown> {
-  public baseUrl: string = 'https://api.campsite.com'
+  public baseUrl: string = 'https://api.gitmono.com'
   private securityData: SecurityDataType | null = null
   private securityWorker?: ApiConfig<SecurityDataType>['securityWorker']
   private abortControllers = new Map<CancelToken, AbortController>()
@@ -4447,7 +4447,7 @@ function dataTaggedQueryKey(key: unknown) {
 /**
  * @title Campsite API
  * @version 2.0.0
- * @baseUrl https://api.campsite.com
+ * @baseUrl https://api.gitmono.com
  * @contact <support@campsite.com>
  */
 export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDataType> {
